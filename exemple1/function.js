49e356f26d558eb953f5a33ff5b75c4b67c13f54
Benoît Thoury <benoit.thoury@gmail.com>
2020-12-18 16:15:33 +0100

    la bonne recherche

--- exemple1/function.js
+++ exemple1/function.js
@@ -16,3 +16,36 @@ $(document).ready(function(){
     });
 
 });
+
+// $(document).ready(function(){
+//     $("form").submit(function(event){
+//         $("input, textarea").each(function(){
+//             if($(this).val().length === 0){
+//                 $(this).css("border", "2px solid red");
+//                 $(this).after("<span>Merci de remplir ce champ.</span>");
+//                 event.preventDefault();
+//             }else{
+//                 let chaine = $("form").serialize();
+//             }
+//         });
+        
+//     });
+// });
+
+// $(document).ready(function () {
+//     $("form").submit(function (event) {
+
+//         isEmpty("#firstname");
+//         isEmpty("#lastname");
+//         isEmpty("#message");
+
+//         function isEmpty(field) {
+//             if ($(field).val().length === 0) {
+//                 $(field).after("<span class='alert'>Merci de remplir ce champ</span>");
+//                 event.preventDefault();
+//             } else {
+//                 let chaine = $("form").serialize();
+//             }
+//         }
+//     });
+// });
