4da0c288f9ac02e2c462fa7fb8d6a52c9491578a
Benoît Thoury <benoit.thoury@gmail.com>
2020-12-18 13:57:14 +0100

    du css

--- function.js
+++ function.js
@@ -1,31 +1,18 @@
 $(document).ready(function(){
+
+
+
     $("form").submit(function(event){
+
         /*Si la longueur de la valeur du champ #prenom est 0 (c'est-à-dire si
         le champ n'a pas été rempli), on affiche un message et on empêche l'envoi*/
-        if($("#prenom").val().length === 0){
-            $("#prenom").after("<span>Merci de remplir ce champ</span>");
+        if($("#prenom").val().length === 0 ){
+            $("#prenom").after("<span class='alert'>Merci de remplir ce champ</span>").css('background-color', 'red');
             event.preventDefault();
         }else{
             //On effectue nos requêtes Ajax, sérialise, etc...
             let chaine = $("form").serialize();
         }
     });
-
-    $("form").submit(function(event){
-        /*Si la longueur de la valeur du champ #prenom est 0 (c'est-à-dire si
-        le champ n'a pas été rempli), on affiche un message et on empêche l'envoi*/
-        if($("#nom").val().length === 0){
-            $("#nom").after("<span>Merci de remplir ce champ</span>");
-            event.preventDefault();
-        }else{
-            //On effectue nos requêtes Ajax, sérialise, etc...
-            let chaine = $("form").serialize();
-        }
-    });
-
-
-
-
-
 
 });
